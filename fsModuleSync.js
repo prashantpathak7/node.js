66a378c7bb97c9e0e27a342aff396d30e81d2892
Prashant Pathak <prashant209997@gmail.com>
2022-01-19 17:47:28 +0530

    Synchronous methods of fs module

---
+++ fsModuleSync.js
@@ -0,0 +1,29 @@
+
+//some synchronous methods of fs module
+const fs = require("fs");
+
+const a = "prashant"
+
+// fs.writeFileSync("read.txt", a);
+// fs.appendFileSync("read.txt", " " + a);
+
+// const buf_data = fs.readFileSync("read.txt");
+// // console.log(buf_data);
+// const org_data = buf_data.toString();
+// console.log(org_data);
+// fs.renameSync('read.txt', 'readwrite.txt')
+
+// fs.mkdirSync("CURD");
+// fs.writeFileSync("CURD/bio.txt", a);
+// fs.renameSync("CURD", "CRUD");
+// fs.appendFileSync("CRUD/bio.txt", "." + " " + "Hello!");
+// const buf_data = toString(fs.readFileSync("CRUD/bio.txt"));
+// const org_data = buf_data.toString();
+// console.log(org_data);
+// const c = fs.readFileSync("CRUD/bio.txt", "utf8")
+// console.log(fs.readFileSync("CRUD/bio.txt", "utf8"));
+// fs.unlinkSync("CRUD/bio.txt");
+// fs.rmdirSync("CRUD");
+// fs.unlinkSync("name.txt");
+// fs.mkdirSync("fsAsync")
+// fs.writeFileSync("fsAsync/index.js", "hello");
