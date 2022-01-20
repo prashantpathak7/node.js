08a0b88d082cef71a17e073d92c0ab8cbb2220cc
Prashant Pathak <prashant209997@gmail.com>
2022-01-20 13:38:26 +0530

    routing

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,9 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.end("Hello from the others sides")
+    if (req.url == "/") {
+        res.end("hello from the home sides");
+    } else if (req.url == "/about") {
+        res.end("Hello from the about sides");
+    } else if (req.url == "/contact") {
+        res.end("Hello from the contact sides");
+    } else {
+        res.writeHead(404, { "Content-type": "text/html" });
+        res.end("<h1>error 404, Page not found</h1>");
+    }
 })
 
-server.listen(8000, "127.0.0.1", () => {
-    console.log("listen to the post no 8000");
+server.listen(8080, "127.0.0.1", () => {
+    console.log("listen to the port no 8080");
 });
