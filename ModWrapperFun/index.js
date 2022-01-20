b96bd22ad9ccea95aa61b68c99a68613acbe165e
Prashant Pathak <prashant209997@gmail.com>
2022-01-20 11:16:47 +0530

    module wrapper function

---
+++ ModWrapperFun/index.js
@@ -0,0 +1,7 @@
+// module wrapper function example
+(function (exports, require, module, __filename, __dirname) {
+    //module code
+    const fs = require("fs");
+    const name = "Prashant";
+    console.log(name);
+});
