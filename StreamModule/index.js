72509b3a0166b5b1161c1b99e5bc404a3fa76771
Prashant Pathak <prashant209997@gmail.com>
2022-01-21 10:09:14 +0530

    Stream Pipes

--- StreamModule/index.js
+++ StreamModule/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const { Stream } = require("stream");
 
 const server = http.createServer();
 
@@ -9,21 +10,25 @@ server.on("request", (req, res) => {
     //     if(err) return console.error(err);
     //     console.log(data.toString());
     // })
-    //////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
 
     // fs.readFile("StreamModule/input.txt", (err, data) => {
     //     if (err) return console.log(err);
     //     res.end(data.toString());
     // })
+    //////////////////////////////////////////////////////////////////////////
+    // const rstream = fs.createReadStream("StreamModule/input.txt");
+
+    // rstream.on("data", (chunkdata) => {
+    //     res.write(chunkdata);
+    // });
+    // rstream.on("end", () => {
+    //     res.end();
+    // })
+    /////////////////////////////////////////////////////////////////////////
 
     const rstream = fs.createReadStream("StreamModule/input.txt");
-
-    rstream.on("data", (chunkdata) => {
-        res.write(chunkdata);
-    });
-    rstream.on("end", () => {
-        res.end();
-    })
+    rstream.pipe(res);
 })
 
 server.listen(8000, "127.0.0.1");
