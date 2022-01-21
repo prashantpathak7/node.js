1d062b29b4c134ee7a7b907455fdfb7776221740
Prashant Pathak <prashant209997@gmail.com>
2022-01-21 10:00:11 +0530

    Stream Module

---
+++ StreamModule/index.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+    // var fs = require("fs");
+    // fs.readFile("input.txt",function(err,data){
+    //     if(err) return console.error(err);
+    //     console.log(data.toString());
+    // })
+    //////////////////////////////////////////////////////////
+
+    // fs.readFile("StreamModule/input.txt", (err, data) => {
+    //     if (err) return console.log(err);
+    //     res.end(data.toString());
+    // })
+
+    const rstream = fs.createReadStream("StreamModule/input.txt");
+
+    rstream.on("data", (chunkdata) => {
+        res.write(chunkdata);
+    });
+    rstream.on("end", () => {
+        res.end();
+    })
+})
+
+server.listen(8000, "127.0.0.1");
