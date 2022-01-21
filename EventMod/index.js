bc21c52dc824e252b5353360796f7330fd89ae81
Prashant Pathak <prashant209997@gmail.com>
2022-01-21 09:28:31 +0530

    Events Module

---
+++ EventMod/index.js
@@ -0,0 +1,20 @@
+//events module
+const EventEmitter = require("events");
+const event = new EventEmitter();
+
+// event.on("sayMyName", () => {
+//     console.log("Your Name is Prashant");
+// })
+// event.on("sayMyName", () => {
+//     console.log("Your Name is Prashant");
+// })
+// event.on("sayMyName", () => {
+//     console.log("Your Name is Prashant");
+// })
+// event.emit("sayMyName");
+
+
+event.on("checkPage", (sc, msg) => {
+    console.log(`status code is ${sc} and the page is ${msg}`);
+});
+event.emit("checkPage", 200, "ok");
